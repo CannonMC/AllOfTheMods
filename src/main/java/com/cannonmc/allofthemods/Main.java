3e150e9a98ab998c8a51f17e64fd103733ca2e2e
ldyte3 <ldyte3@users.noreply.github.com>
2018-06-23 23:49:03 +0100

    Added from intellij 3rd test

--- src/main/java/com/cannonmc/allofthemods/Main.java
+++ src/main/java/com/cannonmc/allofthemods/Main.java
@@ -9,7 +9,6 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class Main {
     public static final String MODID = "allofthemods";
     public static final String VERSION = "1.1";
-    //test comment by luke dyte at 11:42 now. now at 11:45 update
     //new test comment
     
     @EventHandler
