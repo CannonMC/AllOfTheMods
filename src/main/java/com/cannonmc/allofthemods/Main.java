e1e660531a2d90a2fc2b18b5120914a1bb58f056
ldyte3 <ldyte3@users.noreply.github.com>
2018-06-23 23:47:14 +0100

    Added from intellij 3rd test

--- src/main/java/com/cannonmc/allofthemods/Main.java
+++ src/main/java/com/cannonmc/allofthemods/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static final String MODID = "allofthemods";
     public static final String VERSION = "1.1";
     //test comment by luke dyte at 11:42 now. now at 11:45 update
+    //new test comment
     
     @EventHandler
     public void init(FMLInitializationEvent event) {
