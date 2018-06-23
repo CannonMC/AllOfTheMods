07e4f14fdda1d5757f8c1ad49e403015ce261014
ldyte3 <ldyte3@users.noreply.github.com>
2018-06-23 23:46:18 +0100

    Added from intellij 2nd test

--- src/main/java/com/cannonmc/allofthemods/Main.java
+++ src/main/java/com/cannonmc/allofthemods/Main.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class Main {
     public static final String MODID = "allofthemods";
     public static final String VERSION = "1.1";
-    //test comment by luke dyte at 11:42 now.
+    //test comment by luke dyte at 11:42 now. now at 11:45 update
     
     @EventHandler
     public void init(FMLInitializationEvent event) {
