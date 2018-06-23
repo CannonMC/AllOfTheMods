3026c327f3ed6a15df156b7d346d1728cf671840
James Knight <jimdav40@gmail.com>
2018-06-24 00:09:02 +0100

    Change

--- src/main/java/com/cannonmc/allofthemods/Main.java
+++ src/main/java/com/cannonmc/allofthemods/Main.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = Main.MODID, version = Main.VERSION, guiFactory = "com.cannonmc.fullbright.config.ConfigGuiFactory")
+@Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
     public static final String MODID = "allofthemods";
     public static final String VERSION = "1.1";
