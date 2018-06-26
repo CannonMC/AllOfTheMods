326bec48429b74a2efd55d7e4fd965acd6637ddc
James Knight <jimdav40@gmail.com>
2018-06-26 10:59:16 +0100

    Version 0.1

--- src/main/java/com/cannonmc/allofthemods/Reference.java
+++ src/main/java/com/cannonmc/allofthemods/Reference.java
@@ -3,7 +3,7 @@ package com.cannonmc.allofthemods;
 public class Reference {
 	public static final String MODID = "aotm";
     public static final String NAME = "All of the Mods";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.1";
     public static final String SERVER_PROXY_CLASS = "com.cannonmc.allofthemods.proxy.CommonProxy";
     public static final String CLIENT_PROXY_CLASS = "com.cannonmc.allofthemods.proxy.ClientProxy";
 }
