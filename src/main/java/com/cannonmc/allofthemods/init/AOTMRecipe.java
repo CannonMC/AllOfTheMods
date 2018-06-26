dce4970ffeaef1a15d64851cb73d9c317c2b2ce7
James Knight <jimdav40@gmail.com>
2018-06-26 09:26:08 +0100

    The Secret

--- src/main/java/com/cannonmc/allofthemods/init/AOTMRecipe.java
+++ src/main/java/com/cannonmc/allofthemods/init/AOTMRecipe.java
@@ -1,5 +1,7 @@
 package com.cannonmc.allofthemods.init;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -10,12 +12,15 @@ public class AOTMRecipe {
 		//Shaped crafting recipes
 		GameRegistry.addRecipe(new ItemStack(AOTMItems.the_tool), new Object[]{"III", " I ", " I ", 'I', AOTMItems.the_item});
 		GameRegistry.addRecipe(new ItemStack(AOTMBlocks.the_block), new Object[]{"III", "III", "III", 'I', AOTMItems.the_item});
-		
 		GameRegistry.addRecipe(new ItemStack(AOTMBlocks.the_stair, 4), new Object[]{"B  ", "BB ", "BBB", 'B', AOTMBlocks.the_block});
 		GameRegistry.addRecipe(new ItemStack(AOTMBlocks.the_stair, 4), new Object[]{"  B", " BB", "BBB", 'B', AOTMBlocks.the_block});
 		
 		//Shapeless crafting recipes
 		GameRegistry.addShapelessRecipe(new ItemStack(AOTMItems.the_item, 9), new Object[] {new ItemStack(AOTMBlocks.the_block)});
+		
+		GameRegistry.addShapelessRecipe(new ItemStack(AOTMItems.the_secret, 1), new Object[] {new ItemStack(AOTMItems.the_item), new ItemStack(Items.diamond)});
+		GameRegistry.addShapelessRecipe(new ItemStack(AOTMBlocks.secret_stone, 2), new Object[] {new ItemStack(AOTMItems.the_secret), new ItemStack(Blocks.stone)});
+		GameRegistry.addShapelessRecipe(new ItemStack(AOTMBlocks.secret_wood, 2), new Object[] {new ItemStack(AOTMItems.the_secret), new ItemStack(Blocks.planks)});
 		
 		//Smelting
 		GameRegistry.addSmelting(AOTMBlocks.the_ore, new ItemStack(AOTMItems.the_item), 10.0f);
